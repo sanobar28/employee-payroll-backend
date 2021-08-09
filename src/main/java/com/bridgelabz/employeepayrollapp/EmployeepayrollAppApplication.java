0d27cade51c4b1b8600e9376cdbcbf868e3bf908
Namira Mujawar <sanobar.mujawar@gmail.com>
2021-08-09 22:07:00 +0530

    [Sanobar]added: create spring boot project with dependancies

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeepayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeepayrollAppApplication.class, args);
+	}
+
+}

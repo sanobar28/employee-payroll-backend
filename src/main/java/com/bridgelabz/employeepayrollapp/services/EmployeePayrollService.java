2614a0c49da322b3b788cf79de974847ee925e12
Namira Mujawar <sanobar.mujawar@gmail.com>
2021-08-12 15:24:52 +0530

    [Sanobar]added: Database enviornment variables

--- src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employeepayrollapp.services;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
+import com.bridgelabz.employeepayrollapp.exception.EmployeePayrollException;
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
 import com.bridgelabz.employeepayrollapp.repository.EmployeePayrollRepository;
 
@@ -17,17 +18,31 @@ public class EmployeePayrollService implements IEmployeePayrollService {
     @Autowired
     private EmployeePayrollRepository employeePayrollRepository;
 
+    /**
+     * Method to get employee payroll data
+     * @return
+     */
     @Override
     public List<EmployeePayrollData> getEmployeePayrollData() {
         return employeePayrollRepository.findAll();
     }
 
+    /**
+     * Method to get employee data by id
+     * @param empId
+     * @return
+     */
     @Override
     public EmployeePayrollData getEmployeePayrollById(int empId)  {
         return employeePayrollRepository.findById(empId)
                 .orElseThrow(() -> new NotFoundException("User id not found" +empId));
     }
 
+    /**
+     * Method to create employee payroll data
+     * @param empPayrollDTO
+     * @return
+     */
     @Override
     public ResponseDTO createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
        EmployeePayrollData empData = new EmployeePayrollData();
@@ -36,10 +51,16 @@ public class EmployeePayrollService implements IEmployeePayrollService {
        if (empData != null) {
            return new  ResponseDTO("Data inserted successfully!!");
        } else {
-           return new ResponseDTO("insertion failed");
+           throw new EmployeePayrollException("Invalid Data");
        }
     }
 
+    /**
+     * Method to update employee data for given id
+     * @param empID
+     * @param empPayrollDTO
+     * @return
+     */
     @Override
     public EmployeePayrollData updateEmployeePayrollData(int empID,
                                                          EmployeePayrollDTO empPayrollDTO)  {
@@ -50,12 +71,23 @@ public class EmployeePayrollService implements IEmployeePayrollService {
         return employeePayrollRepository.save(empData);
     }
 
+    /**
+     * Method to delete employee data by id
+     * @param empId
+     * @return
+     */
     @Override
     public ResponseDTO deleteEmployeePayrollData(int empId) {
         employeePayrollRepository.deleteById(empId);
         return new ResponseDTO("Deleted Successfully");
     }
 
+    /**
+     * Method to convert employee payroll data to transfer object
+     * @param empData
+     * @param employeePayrollDTO
+     * @return
+     */
     private EmployeePayrollData convertEntity (EmployeePayrollData empData, EmployeePayrollDTO employeePayrollDTO) {
         empData.setName(employeePayrollDTO.getName());
         empData.setDepartment(employeePayrollDTO.getDepartment());
